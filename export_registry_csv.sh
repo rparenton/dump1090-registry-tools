3553c280c160e030423cd3ba00c11123b9e354f4
Robert Parenton <rparenton@users.noreply.github.com>
2016-09-30 18:05:34 -0500

    Added British Aerospace/BAe, British Aircraft Corp/BAC, Cirrus, Dassault, Folland, Gulfstream, Handley Page, Hunting, and Raytheon ICAO type mappings.  Also added type overrides for BLCF and BSCA aircraft registrations to the export script.

--- export_registry_csv.sh
+++ export_registry_csv.sh
@@ -2,7 +2,20 @@
 
 WHOAMI=`whoami`
 
+# Export CSV files
 mysql -p < export_registry_csv.sql
 mv /var/lib/mysql/dump1090/*.csv .
 chown $WHOAMI:$WHOAMI *.csv
 chmod 600 *.csv
+
+### ICAO type overrides ###
+
+# BLCF (Dreamlifter)
+sed -i 's/N249BA,B744/N249BA,BLCF/g' faa_registry.csv
+sed -i 's/N718BA,B744/N718BA,BLCF/g' faa_registry.csv
+sed -i 's/N747BC,B744/N747BC,BLCF/g' faa_registry.csv
+sed -i 's/N780BA,B744/N780BA,BLCF/g' faa_registry.csv
+
+# BSCA (Shuttle Carrier Aircraft)
+sed -i 's/N905NA,B741/N905NA,BSCA/g' faa_registry.csv
+sed -i 's/N911NA,B74R/N911NA,BSCA/g' faa_registry.csv
